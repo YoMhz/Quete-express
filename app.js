99da0b257072776fe5a2b66be08437e350ce3e5c
Yohann Mahrez <yohann.mahrez@gmail.com>
2021-12-06 12:04:10 +0100

    Ajout architecture MVC

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-// Setup the environement variables form a .env file
-require('dotenv').config();
-
-// Import expres
+const { setupRoutes } = require('./routes');
 const express = require('express');
-
-// We store all express methods in a variable called app
 const app = express();
 
-// If an environment variable named PORT exists, we take it in order to let the user change the port without chaning the source code. Otherwise we give a default value of 3000
 const port = process.env.PORT || 3000;
 
-// We listen to incoming request on the port defined above
+app.use(express.json());
+
+setupRoutes(app);
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const moviesRouter = require('./movies');
+const usersRouter = require('./users');
+
+const setupRoutes = (app) => {
+  
+  app.use('/api/movies', moviesRouter);
+  
+  app.use('/api/users', usersRouter);
+};
+
+module.exports = {
+  setupRoutes,
+};
